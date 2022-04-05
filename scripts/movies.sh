77d87d31c777ff238c9f36d1cea29cc9b9ba4a76
jngaravitoc <jngaravitoc@email.arizona.edu>
2022-04-05 10:11:06 -0400

    loading ffmpeg

--- scripts/movies.sh
+++ scripts/movies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+module load ffmpeg
+
 frame_rate='15'
-video_name='OP_MWLMC5_b0_dens_0.mp4'
-ffmpeg  -r $frame_rate -i ./OP_MWLMC5_100M_b0_OM3_%3d.png -s 1920x1080 -vcodec libx264 -crf 15 -pix_fmt yuv420p $video_name
+video_name='OP_MWLMC5_b0_over_density.mp4'
+file_name='../mwlmc5_over_density/MWLMC5_over_density_100M_b0_OM3_'
+ffmpeg  -r $frame_rate -i $file_name%3d.png -s 1920x1080  -crf 15 -pix_fmt yuv420p $video_name
 
